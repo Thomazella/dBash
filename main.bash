e58f766babf5e86859a9b3e0457963dd5e8fca84
Raphael Thomazella <thomazella9@gmail.com>
2018-08-05 05:24:33 -0300

    local fixes, added hardnary

--- main.bash
+++ main.bash
@@ -45,8 +45,7 @@ truthy() {
 
 dotest() {
   [ $# == 0 ] && return 1
-  local predicate="$@"
-  local isNotTruthyTestOrInvalid='^-[[:alpha:]][ ][^ ]+|[^ ]+[ ]-[[:alpha:]][[:alpha:]][ ][^ ]+|[^ ]+[ ]=[ ][^ ]+|[^ ]+[ ]==[ ][^ ]+|[^ ]+[ ]!=[ ][^ ]+|[^ ]+[ ]<[ ][^ ]+|[^ ]+[ ]>[ ][^ ]+'
+  local predicate="$@" isNotTruthyTestOrInvalid='^-[[:alpha:]][ ][^ ]+|[^ ]+[ ]-[[:alpha:]][[:alpha:]][ ][^ ]+|[^ ]+[ ]=[ ][^ ]+|[^ ]+[ ]==[ ][^ ]+|[^ ]+[ ]!=[ ][^ ]+|[^ ]+[ ]<[ ][^ ]+|[^ ]+[ ]>[ ][^ ]+'
   if [[ "$predicate" =~ $isNotTruthyTestOrInvalid ]]
   then [ $predicate ] 2>/dev/null && return 0
   fi
@@ -57,10 +56,7 @@ dotest() {
 
 ternary() {
   [ $# != 3 ] && return 1
-  local condition="$1"
-  local pass="$2"
-  local fail="$3"
-  local silent=">/dev/null 2>&1"
+  local condition="$1" pass="$2" fail="$3" silent=">/dev/null 2>&1"
   if ! which $pass $silent; then pass="printf $pass"; fi
   if ! which $fail $silent; then fail="printf $fail"; fi
   if [ "$condition" == true ]; then eval $pass && return; fi
@@ -98,8 +94,7 @@ trim() {
 
 mute() {
   [ "$#" == 0 ] && return 1
-  local noStdout=1\>/dev/null
-  local noStderr=2\>/dev/null
+  local noStdout=1\>/dev/null noStderr=2\>/dev/null
   eval "$@" $noStdout $noStderr
 }
 
@@ -130,6 +125,31 @@ or() {
     [ "$?" == 0 ] && return 0
   done
   return 1
+}
+
+hardnary() {
+  [ $# -lt 5 ] && return 1
+	[[ ! "$@" =~ [[:space:]]+[?][[:space:]]+[^:]+[[:space:]]+[:][[:space:]]+ ]] && return 1
+
+	local condition pass fail passCommand failCommand
+	for arg in $@; do
+		[ $arg == "?" ] && local mark=true && continue
+		[ $arg == ":" ] && local colon=true && continue
+		[ ! $mark -a ! $colon ] && condition="$condition $arg" && continue
+		[ $mark -a ! $colon ] && pass="$pass $arg" && continue
+		fail="$fail $arg"
+	done
+
+	[[ "$pass" =~ ^[[:space:]]*([^ ]+) ]] && passCommand=${BASH_REMATCH[1]}
+	[[ "$fail" =~ ^[[:space:]]*([^ ]+) ]] && failCommand=${BASH_REMATCH[1]}
+  if ! which $passCommand >/dev/null 2>&1; then pass="printf \"$pass\""; fi
+  if ! which $failCommand >/dev/null 2>&1; then fail="printf \"$fail\""; fi
+  if [[ "$condition" =~ [[:space:]]*true[[:space:]]* ]]; then eval $pass && return; fi
+  if [[ "$condition" =~ [[:space:]]*false[[:space:]]* ]]; then eval $fail && return; fi
+  if dotest $condition
+  then eval $pass
+  else eval $fail
+  fi
 }
 
 #--------------
--- test/main.test.bats
+++ test/main.test.bats
@@ -127,45 +127,45 @@
   if dotest 1 -gt 20; then false; else true; fi
 }
 
-@test "ternary exits 1 when not given 3 args" {
-  run ternary 1
+@test "hardnary exits 1 when not given 5 args" {
+  run hardnary 1
   [ $status == 1 ]
-  run ternary 1 2
+  run hardnary 1 2
   [ $status == 1 ]
-  run ternary 1 2 3 4
+  run hardnary 1 2 3 4
   [ $status == 1 ]
 }
 
-@test "ternary '1 == 1' y n" {
-  run ternary '1 == 1' y n
+@test "hardnary 1 == 1 ? y : n" {
+  run hardnary 1 == 1 ? y : n
   [ $output == "y" ]
 }
 
-@test "ternary '1 == 10' y n" {
-  run ternary '1 == 10' y n
+@test "hardnary 1 == 10 ? y : n" {
+  run hardnary 1 == 10 ? y : n
   [ $output == "n" ]
 }
 
-@test "ternary true y n" {
-  run ternary true y n
+@test "hardnary true ? y : n" {
+  run hardnary true ? y : n
   [ $output == "y" ]
 }
 
-@test "ternary false y n" {
-  run ternary false y n
+@test "hardnaryt false ? y : n" {
+  run hardnary false ? y : n
   [ $output == "n" ]
 }
 
-@test "ternary '\$var == \$var' y n" {
+@test "hardnary \$var == \$var ? y : n" {
   local a=1
   local b=2
-  run ternary "$a == $b" y n
+  run hardnary $a == $b ? y : n
   [ $output == "n" ]
 }
 
 @test "ternary in var declaration" {
-  local b=$(ternary true 99 00)
+  local b=$(hardnary true ? 99 : 00)
   [ $b == 99 ]
 }
 
-# rewrite ternary to use ? : as argument separators
+# rewrite hardnary to use ? : as argument separators
